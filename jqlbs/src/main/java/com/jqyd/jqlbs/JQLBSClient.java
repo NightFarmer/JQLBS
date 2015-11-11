f9a924140d5f3713bd57b0876964fecca56a0235
NightFarmer <494090597@qq.com>
2015-11-11 14:28:00 +0800

    优化定位

--- jqlbs/src/main/java/com/jqyd/jqlbs/JQLBSClient.java
+++ jqlbs/src/main/java/com/jqyd/jqlbs/JQLBSClient.java
@@ -26,13 +26,8 @@ public class JQLBSClient {
 //    final static String url = "http://www.jqgj.com.cn:9090/jqgj_server_client/login!lxsb_new.action";
 //                    String url = "http://192.168.1.80:8080/jqgj_2.0_serv/login!lxsb_new.action";
 
-    /**
-     * 请求久其定位接口，定位成功与否都直接提交久其服务器
-     * 若提交失败则保持至本地，下次联网时一同提交
-     *
-     * @param callBack 百度定位成功回调
-     */
-    public static void upLoadLocation(final Context context, final LBSParam lbsParam, final LocationUpLoadCallBack callBack) {
+
+    private static void location(final Context context, final LBSParam lbsParam, final LocationUpLoadCallBack callBack, final BDLocationListener locationListener) {
         if (Looper.getMainLooper() != Looper.myLooper()) {
             throw new RuntimeException("不能在子线程中调用该方法");
         }
@@ -41,10 +36,16 @@ public class JQLBSClient {
             BDLocationListener bdLocationListener = new BDLocationListener() {
                 @Override
                 public void onReceiveLocation(BDLocation bdLocation) {
-                    new Thread(new UpLoadLocRunnable(bdLocation, context, lbsParam)).start();
+                    if (lbsParam != null) {
+                        new Thread(new UpLoadLocRunnable(bdLocation, context, lbsParam)).start();
+                    }
                     if (callBack != null) {
                         callBack.onResult(bdLocation.getLocType());
                     }
+                    if (locationListener != null) {
+                        locationListener.onReceiveLocation(bdLocation);
+                    }
+                    mLocationClient.stop();
                 }
             };
             mLocationClient.registerLocationListener(bdLocationListener);
@@ -53,13 +54,28 @@ public class JQLBSClient {
             locOption.setLocationMode(LocationClientOption.LocationMode.Hight_Accuracy);//高精度
             mLocationClient.setLocOption(locOption);
         }
-
-        if (!mLocationClient.isStarted()) {
+//        if (!mLocationClient.isStarted()) {
             mLocationClient.start();
-        } else {
-            int result = mLocationClient.requestLocation();
-            Log.i("xx", "result" + result);
-        }
+//        } else {
+//            int result = mLocationClient.requestLocation();
+//            Log.i("xx", "result" + result);
+//        }
+    }
+
+
+    public static void requestLocation(Context context, @NonNull BDLocationListener bdLocationListener) {
+        location(context, null, null, bdLocationListener);
+    }
+
+
+    /**
+     * 请求久其定位接口，定位成功与否都直接提交久其服务器
+     * 若提交失败则保持至本地，下次联网时一同提交
+     *
+     * @param callBack 百度定位成功回调
+     */
+    public static void upLoadLocation(final Context context, final LBSParam lbsParam, final LocationUpLoadCallBack callBack) {
+        location(context, lbsParam, callBack, null);
     }
 
     /**
@@ -100,9 +116,9 @@ public class JQLBSClient {
             if (serverTime != 0) {
                 locationBean.time = serverTime;
             }
-            if (locationBean.time - preUploadTime < 50 * 1000) {
-                return;
-            }
+//            if (locationBean.time - preUploadTime < 50 * 1000) {
+//                return;
+//            }
             preUploadTime = locationBean.time;
 
             switch (locationBean.loc_method) {
